5579edd82d8fe24ce3feb86a3c6ca4bc5e93f0d2
YILDIRM225 <92109800+YILDIRM225@users.noreply.github.com>
2021-10-19 13:12:01 -0500

    Add files via upload

---
+++ lab 2.py
@@ -0,0 +1,39 @@
+#NAME-CONNER MARTINEZ
+#date-10,12,2021
+#1
+print("hello world")
+#2
+hi=("hey there")
+name=(" conner")
+greeting=hi+name
+print(greeting)
+#3
+hi=("hey there")
+name=(" professor sunham num")
+names=(" and conner")
+greeting= hi+ name + names
+print(greeting)
+#4
+r= float(input ("Input the radius of the circle :360 "))
+#5
+m=int("65")
+g=int("7")
+mpg=m/g
+print(mpg)
+#6
+f = int(" 65")
+c= (f-32) * 5/9
+print(c)
+#7
+start =int("3")
+length= int("10")
+x=start + length
+y= x%7
+print(y)
+
+
+
+
+
+      
+
